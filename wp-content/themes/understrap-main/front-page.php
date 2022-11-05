b7484fea9e991747ec127f8b6eb0b9afc6c60992
Sagou Mohamed <95595076+medsagou@users.noreply.github.com>
2022-11-05 23:53:11 +0100

    repensive font-sizes small screens

--- wp-content/themes/understrap-main/front-page.php
+++ wp-content/themes/understrap-main/front-page.php
@@ -23,8 +23,8 @@
                         <div class="">
                             <div class="">
                                 <div>
-                                    <h1 class="text-center text-lg-start  me-lg-5">Increase Your Sales With Our High Quality <span class="">Leads</span></h1>
-                                    <p class="text-center text-lg-start w-75 m-auto m-lg-0">There are over 30 million Small Businesses in the United States. Knowing their financial and insurance needs is critical to your wining their business.</p>
+                                    <h1 class="text-center text-lg-start mx-auto  me-lg-5">Increase Your Sales With Our High Quality <span class="">Leads</span></h1>
+                                    <p class="text-center text-lg-start  text-info w-75 m-auto m-lg-0">There are over 30 million Small Businesses in the United States. Knowing their financial and insurance needs is critical to your wining their business.</p>
                                     <div class="d-none d-lg-block mt-4 mb-4">
                                         <ul class="list-group list-group-horizontal p-0 display-6">
                                             <li class="list-group-item"> <span><i class="fa-solid fa-check text-primary me-2 pe-2"></i></span> Targeted Data</li>
@@ -35,16 +35,16 @@
                                     <div class="mt-4 text-center text-lg-start">
                                         <button type="button" class="btn btn-primary rounded-pill"><a class="text-secondary" href="#">Learn More<i class="fa-solid fa-arrow-right-long ms-3"></i></a></button>
                                     </div>
-                                    <div class="d-lg-flex flex-row position-relative mt-5">
-                                        <div class="p-0 me-lg-1 text-center text-lg-start mt-4">
+                                    <div class="d-flex flex-row position-relative mt-5 px-3 px-lg-0 justify-content-md-center justify-content-lg-start">
+                                        <div class="p-0 me-lg-1 mt-4">
                                             <h3 class="text-primary mb-0 me-lg-5">6Millions</h3>
                                             <p class="display-4">Benefited Businesses</p>
                                         </div>
-                                        <div class="p-0 mx-lg-5 text-center text-lg-start mt-4">
+                                        <div class="p-0 mx-lg-5 mx-3 mt-4">
                                             <h3 class="text-primary mb-0 me-lg-5 pe-2">$280B</h3>
                                             <p class="display-4">PPP Loans Funded</p>
                                         </div>
-                                        <div class="p-0 me-lg-5 text-center text-lg-start mt-4">
+                                        <div class="p-0 me-md-3 me-lg-5 mt-4">
                                             <h3 class="text-primary mb-0 me-lg-5 pe-2">$416.3B</h3>
                                             <p class="display-4 fw-normal">Emergency Relief Aid</p>
                                         </div>
@@ -67,7 +67,7 @@
             </div>
             <div class="col-lg mb-4 text-white d-flex flex-column justify-content-center  text-center text-lg-start">
                 <h5 class="text-primary m-0">AI driven qualified</h5>
-                <h1 class=" mb-4">Small Business Leads</h1>
+                <h1 class="mx-auto mx-lg-0 mb-4">Small Business Leads</h1>
                 <p class=" mx-auto w-75 me-lg-5 ms-lg-0 pe-lg-5 text-info">We deliver AI driven qualified Small Business Leads with high-propensity to buy that the Banks and Insurance companies can leverage for prospecting.</p>
 
                 <div class="mt-5">
